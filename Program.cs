5ce61e4cab413e33081db83e59b5e878c6f6985a
BuildTools <unconfigured@null.spigotmc.org>
2021-09-07 11:03:21 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _321PA1_abrush60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
